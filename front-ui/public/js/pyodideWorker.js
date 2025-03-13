c29c5afb3f8f43c975aa5e3ff1ba500ddc8bfffd
wangxinpo <1124434059@qq.com>
2025-03-13 18:39:19 +0800

    样式修复，提示词优化

---
+++ front-ui/public/js/pyodideWorker.js
@@ -0,0 +1,23 @@
+self.importScripts('/js/pyodide/pyodide.js')
+const pyodide = loadPyodide().then((p) => {
+  self.postMessage({ result: 'Pyodide 加载完成' })
+  p.loadPackage(['micropip', 'numpy', 'scikit-learn', 'scipy', 'pandas'])
+  return p
+})
+self.onmessage = async (event) => {
+  const { pythonCode } = event.data
+  runPythonCode(await pyodide, pythonCode)
+}
+const runPythonCode = (pyodide, pythonCode) => {
+  if (!pythonCode) {
+    self.postMessage({ error: 'Python 代码为空' })
+    return
+  }
+  try {
+    const result = pyodide.runPython(pythonCode)
+    // 转换为可序列化的 JavaScript 数据
+    self.postMessage({ result: result.toString() ? result.toString() : result })
+  } catch (error) {
+    self.postMessage({ error: error.message })
+  }
+}
